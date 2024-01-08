1b81aecc2cc92573e92ff52c8efaceece8aa4c8c
Samuel Rivera <samuel.rivera@mrjaffesclass.com>
2024-01-08 11:42:15 -0800

    up to gamestate

---
+++ tictactoe/State.java
@@ -0,0 +1,14 @@
+package tictactoe;
+
+/**
+ * Tic-Tac-Toe state variables.
+ */
+public class State
+{
+    private int gameState = Constants.STANDBY;
+    private int whoseMove = Constants.X;
+    private String xName = "";
+    private String oName = "";
+    private int[][] board = new int[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
+}
+
